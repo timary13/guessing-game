97f624d158c20787ddc9082fdcb0780d107fa62f
timary13 <timary99@gmail.com>
2019-10-08 14:53:01 +0300

    fear: add task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
+const chai = require('../test/setup-mocha');
+
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this._range = null;
+        //middle position
+        this._middle = 0;
+    }
+
+    display() {
+        console.log(this._range);
+    }
 
     setRange(min, max) {
-
+        this._range = Array(max - min).fill().map((item, i) => min + i);
+        this._middle = Math.ceil(this._range.length / 2);
     }
 
     guess() {
-
+        return this._range[this._middle];
     }
 
     lower() {
-
+        this._range = this._range.slice(0, this._middle);
+        this._middle = Math.ceil(this._range.length / 2);
     }
 
     greater() {
-
+        this._range = this._range.slice(this._middle);
+        this._middle = Math.ceil(this._range.length / 2);
     }
 }
 
